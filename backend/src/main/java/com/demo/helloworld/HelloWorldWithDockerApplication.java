ff3e35bd292c23c3f24b8e12c36a143aeacddf14
Deepanwita Sahu <deepanwita.sahu@thoughtworks.com>
2025-05-13 15:59:47 +0530

    Initial commit: Dockerized full stack app with React and Spring Boot

---
+++ backend/src/main/java/com/demo/helloworld/HelloWorldWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWithDockerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/helloworld/HelloWorldWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
